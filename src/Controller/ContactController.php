aa7e67eb4b2d7f931298809e1244033f47462cc7
stephane Joos <stephane.joos@gmail.com>
2018-12-14 11:39:12 +0100

    Add some stuff

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\CmsPages;
+use App\Repository\CmsPagesRepository;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
-    {
+    public function index(CmsPagesRepository $repo)
+    {        
+        $pageContent = $repo->findOneBy(['CmsCategory'=>'contact']);
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'content' => $pageContent
         ]);
     }
 }
